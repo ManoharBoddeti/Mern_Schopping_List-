e5e31c6d3af64344ea7fb8b61837fb71fbe39ca1
ManoharBoddeti <manoharboddeti8252@gmail.com>
2021-10-13 11:36:25 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './actions/ItemModal';
+import { Container } from 'reactstrap';
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
-  return (
+import { loadUser } from './actions/authActions';
+
+
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  render(){
+  return ( 
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppNavbar />
+      <Container>
+      <ItemModal />
+      <ShoppingList />  
+      </Container>                                                               
     </div>
+    </Provider>
   );
+}
 }
 
 export default App;
